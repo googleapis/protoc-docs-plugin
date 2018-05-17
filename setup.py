97140ce010d189d0d89969b966e35455817e7915
Luke Sneeringer <luke@sneeringer.com>
2018-05-17 11:06:11 -0700

    Version number bump. (#7)

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ import setuptools
 
 setuptools.setup(
     name='protoc-docs-plugin',
-    version='0.2.0',
+    version='0.3.0',
     description='Plugin for reading and writing documentation from '
                 'protobuf files into existing generated protoc output.',
     author='Luke Sneeringer',
